874eedd03ae108bf509a44e5a02d8437db3ee761
amitraj-123 <31385544+amitraj-123@users.noreply.github.com>
2023-05-10 23:11:43 +0530

    Test case added for string with whitespace

--- addCalc.test.ts
+++ addCalc.test.ts
@@ -3,4 +3,7 @@ describe('testing add method', () => {
   it('should give zero for empty string', () => {
     expect(add("")).toBe(0);
   });
+  it('should give zero for whitespace in string', () => {
+    expect(add(" ")).toBe(0);
+  });
 });
